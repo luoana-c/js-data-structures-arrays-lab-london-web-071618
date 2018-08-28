2b3ce074ac351d94ea01ca396cc16b3ab37b9470
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-28 13:55:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,8 @@ function appendDriver(name) {
   newDrivers = [...drivers, name]
   return newDrivers
 }
+
+function prependDriver(name) {
+  newDrivers = [name, ...drivers]
+  return newDrivers
+}
