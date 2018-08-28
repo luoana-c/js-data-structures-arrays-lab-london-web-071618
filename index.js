5760950a2fa2f7fde686fde859d70ed5949e0cb7
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-28 13:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,3 +35,8 @@ function removeLastDriver() {
   const newDrivers = drivers.slice(0, (drivers.length -1))
   return newDrivers
 }
+
+function removeFirstDriver() {
+  const newDrivers = drivers.slice(1)
+  return newDrivers
+}
